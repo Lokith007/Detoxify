1b3c794a7b06da756b4a4a0c5b2ecb1558352383
Pranatheesh-S <pranatheesh@gmail.com>
2025-06-14 15:31:51 +0530

    Database establishment

---
+++ models/users.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const connect = mongoose.connect("mongodb://localhost:27017/Detoxify");
+
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gmail : {
+        type : String,
+        required: true
+    },
+    lastlogin:{
+        type: Date,    
+        default: () => new Date().toISOString().split('T')[0]
+
+    },
+    streak:{
+            type:Number,
+            default:0
+    },
+    XP:{
+        type:Number,
+        default:0
+    },
+    Apartner:{
+    type:String,
+    }
+});
+
+
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
