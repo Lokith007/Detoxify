8e292edf40b3a5d63e7c35051f4a77506e3c8449
DHANVANTHGS <dhanvanthgs25@gmail.com>
2025-06-15 23:23:09 +0530

    Update alertHandler.js
    alerthandler updated

--- Backend/handlers/alertHandler.js
+++ Backend/handlers/alertHandler.js
@@ -2,7 +2,10 @@ const express=require("express");
 const bodyparser= require('body-parser');
 const webpush= require('web-push');
 const expressAsyncHandler=require('express-async-handler');
+const cookieParser=require('cookie-parser');
+const jwt=require('jsonwebtoken'); 
 const Users=require('../../models/users');
+const path = require('path');
 const ambulance = require('../../models/ambulance');
 
 const app=express();
@@ -11,16 +14,20 @@ app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname,'..','..','Frontend')));
 
 const sos = expressAsyncHandler(async(req,res)=>{
+    console.log(1);
     const token = req.cookies.token;
     const decoded_token=jwt.verify(token,process.env.secretkey);
     const mail=decoded_token.mail;
     const lat= req.body.latitude;
     const long =req.body.longitude;
-    const user=Users.findOne({gmail:mail});
+    const user= await Users.findOne({gmail:mail});
+    if(!user){
+         return res.status(404).send(false);
+    }
     const message = "Emergency Alert !";
     const payload = JSON.stringify({
             title: message,
-            body: `${data.name} needs an immediate help at the location below ,call to : ${user.number}` ,
+            body: `${user.name} needs an immediate help at the location below ,call to : ${user.number}` ,
             data: {
                 contact_number : user.number,
                 lat,
@@ -36,6 +43,8 @@ const sos = expressAsyncHandler(async(req,res)=>{
            webpush.sendNotification(amb.subscription,payload);
         }
     });
+    console.log('sent');
+    res.send(true);
 });
 
 const getkey = ((req,res) =>{
